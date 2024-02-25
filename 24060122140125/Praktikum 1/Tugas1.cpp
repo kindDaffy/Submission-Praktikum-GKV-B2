f4404e1c8089bad68bbc7239db79e8b31fcb2e61
anandabayu <160461302+anandabayu2611@users.noreply.github.com>
2024-02-26 02:04:30 +0700

    Update Tugas1.cpp

--- 24060122140125/Praktikum 1/Tugas1.cpp
+++ 24060122140125/Praktikum 1/Tugas1.cpp
@@ -1,16 +1,16 @@
 /*
 Ananda Putra Bayu	
 24060122140125
- */
+*/
 
 #include <GL/glut.h>
 #include <stdlib.h>
 void
 Muka(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT);
 
-    // Gambar kepala 
+// Gambar kepala 
     glBegin(GL_QUADS);
     glColor3f(1.0, 0.8, 0.6); // Warna kulit
     glVertex2f(-0.4, -0.4);
@@ -19,7 +19,7 @@ Muka(void)
     glVertex2f(-0.4, 0.5);
     glEnd();
 
-    // Gambar mata kiri 
+ // Gambar mata kiri 
     glBegin(GL_QUADS);
     glColor3f(0.0, 0.0, 0.0); // Warna hitam
     glVertex2f(-0.2, 0.1);
@@ -28,7 +28,7 @@ Muka(void)
     glVertex2f(-0.2, 0.2);
     glEnd();
 
-    // Gambar mata kanan
+ // Gambar mata kanan
     glBegin(GL_QUADS);
     glColor3f(0.0, 0.0, 0.0); // Warna hitam
     glVertex2f(0.1, 0.1);
@@ -37,7 +37,7 @@ Muka(void)
     glVertex2f(0.1, 0.2);
     glEnd();
 
-    // Gambar mulut 
+// Gambar mulut 
     glBegin(GL_QUADS);
     glColor3f(0.8, 0.0, 0.0); // Warna merah
     glVertex2f(-0.15, -0.2);
@@ -46,15 +46,15 @@ Muka(void)
     glVertex2f(-0.15, -0.1);
     glEnd();
     
-	// Gambar hidung
-	glPointSize(5.0f);
-	glBegin(GL_POINTS);
-	glColor3f(1.0, 1.0, 1.0);
-	glVertex3f(0.0, 0.0, 0.0);
-	glEnd();
+ // Gambar hidung
+    glPointSize(5.0f);
+    glBegin(GL_POINTS);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glEnd();
 	
-	// Gambar Alis Mata Kiri
-	glBegin(GL_QUADS);
+// Gambar Alis Mata Kiri
+    glBegin(GL_QUADS);
     glColor3f(0.0, 0.0, 0.0); // Warna hitam
     glVertex2f(-0.25, 0.3);
     glVertex2f(-0.05, 0.3);
@@ -62,15 +62,14 @@ Muka(void)
     glVertex2f(-0.25, 0.4);
     glEnd();
     
-    // Gambar Alis Mata Kanan
+ // Gambar Alis Mata Kanan
     glBegin(GL_QUADS);
     glColor3f(0.0, 0.0, 0.0); // Warna hitam
-	glVertex2f(0.05, 0.3);
+    glVertex2f(0.05, 0.3);
     glVertex2f(0.25, 0.3);
     glVertex2f(0.25, 0.4);
     glVertex2f(0.05, 0.4);
 	
-    
     glEnd();	glFlush();
     
 }	
@@ -81,7 +80,7 @@ int main(int argc, char*argv[])
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 	glutCreateWindow("Muka");
 	glutDisplayFunc(Muka);	
-	glClearColor (0.1f, 0.0f, 0.1f, 1.0f);
+	glClearColor (0.1f, 0.0f, 0.1f, 1.0f); // Warna ungu
 	glutMainLoop();
 return 0;
 }
