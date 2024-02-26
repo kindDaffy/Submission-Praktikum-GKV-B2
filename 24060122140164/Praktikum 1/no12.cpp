f18ecfdad341876a69d66e04a722ab3568bb0876
Al Ferro Putra Yusanda <79499749+Ferituaku@users.noreply.github.com>
2024-02-26 11:32:34 +0700

    Update no12.cpp

--- 24060122140164/Praktikum 1/no12.cpp
+++ 24060122140164/Praktikum 1/no12.cpp
@@ -1,81 +1,49 @@
 #include <GL/glut.h>
-
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // GL_LINE_STRIP
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glBegin(GL_LINE_STRIP);
-    glVertex2f(-0.1, -0.1);
-    glVertex2f(0.0, 0.1);
-    glVertex2f(0.1, -0.1);
-    glEnd();
-
-    // GL_LINE_LOOP
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(-0.4, -0.4);
-    glVertex2f(0.0, 0.4);
-    glVertex2f(0.4, -0.4);
-    glEnd();
-
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(-0.3, -0.3);
-    glVertex2f(0.0, 0.3);
-    glVertex2f(0.3, -0.3);
-    glEnd();
-
-    // GL_TRIANGLE_FAN
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(-0.5, -0.1);
-    glVertex2f(0.0, 0.1);
-    glVertex2f(0.5, -0.1);
-    glEnd();
-
-    // GL_TRIANGLE_STRIP
- 	//glColor3f(1.0f, 1.0f, 0.0f);
-    //glBegin(GL_TRIANGLE_STRIP);
-    //glVertex2f(-0.2, -0.1);
-    //glVertex2f(0.0, 0.1);
-    //glVertex2f(0.3, -0.1);
-    //glVertex2f(0.2, 0.1);
-    //glEnd();
-
-    // GL_QUADS
-    glColor3f(1.0f, 0.0f, 1.0f);
+    // House Base (GL_QUADS)
+    glColor3f(0.1f, 0.4f, 0.4f);
     glBegin(GL_QUADS);
-    glVertex2f(-0.1, -0.1);
-    glVertex2f(0.0, 0.1);
-    glVertex2f(0.1, -0.1);
-    glVertex2f(0.1, 0.1);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glVertex2f(0.5, 0.5);
+    glVertex2f(-0.5, 0.5);
     glEnd();
 
-    // GL_QUAD_STRIP
-    glColor3f(0.5f, 0.5f, 0.5f);
-    glBegin(GL_QUAD_STRIP);
-    glVertex2f(-0.1, -0.1);
-    glVertex2f(0.0, 0.1);
-    glVertex2f(0.1, -0.1);
-    glVertex2f(0.2, 0.1);
-    glEnd();
-
-    glColor3f(0.8f, 0.2f, 0.5f);
-    glBegin(GL_POLYGON);
-    glVertex2f(-0.2, -0.4);
-    glVertex2f(0.1, -0.3);
-    glVertex2f(0.2, -0.5);
-    glVertex2f(0.0, -0.6);
-    glVertex2f(-0.1, -0.5);
-    glEnd();
-
-    glColor3f(0.5f, 0.5f, 0.2f);
+    // Roof (GL_TRIANGLES)
+    glColor3f(0.9f, 0.2f, 0.2f);
     glBegin(GL_TRIANGLES);
-    glVertex2f(-0.3, 0.3);
-    glVertex2f(0.0, 0.6);
-    glVertex2f(0.3, 0.3);
+    glVertex2f(-0.5, 0.5);
+    glVertex2f(0.0, 1.0);
+    glVertex2f(0.5, 0.5);
+    glEnd();
+
+    // Door (GL_QUADS)
+    glColor3f(0.4f, 0.2f, 0.1f);
+    glBegin(GL_QUADS);
+    glVertex2f(-0.1, -0.5);
+    glVertex2f(0.1, -0.5);
+    glVertex2f(0.1, 0.0);
+    glVertex2f(-0.1, 0.0);
+    glEnd();
+
+    // Window 1 (GL_QUADS)
+    glColor3f(0.3f, 0.7f, 0.9f);
+    glBegin(GL_QUADS);
+    glVertex2f(-0.4, -0.2);
+    glVertex2f(-0.3, -0.2);
+    glVertex2f(-0.3, -0.1);
+    glVertex2f(-0.4, -0.1);
+    glEnd();
+
+    // Window 2 (GL_QUADS)
+    glColor3f(0.3f, 0.7f, 0.9f);
+    glBegin(GL_QUADS);
+    glVertex2f(0.3, -0.2);
+    glVertex2f(0.4, -0.2);
+    glVertex2f(0.4, -0.1);
+    glVertex2f(0.3, -0.1);
     glEnd();
 
     glFlush();
@@ -86,7 +54,7 @@ void display(void) {
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutCreateWindow("2D OpenGL Primitives");
+    glutCreateWindow("Rumah Kita");
 	glutInitWindowSize(640, 480);
     glutDisplayFunc(display);
    
@@ -94,4 +62,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
